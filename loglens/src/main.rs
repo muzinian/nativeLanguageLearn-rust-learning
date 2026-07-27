37c77ae804e73c0e1c8748d4cf03c6d870f84b02
licheng <chengli@deepglint.com>
2026-07-27 16:25:04 +0800

    实现解析命令行参数,返回匹配行数

--- loglens/src/main.rs
+++ loglens/src/main.rs
@@ -17,22 +17,35 @@ fn main() {
             std::process::exit(2);
         }
     };
+    let ignore_case = match args.next() {
+        Some(flag) => {
+            if flag == "--ignore-case" {
+                true
+            } else {
+                eprintln!("未知的标志：{flag}");
+                std::process::exit(2);
+            }
+        }
+        None => false,
+    };
+
     match fs::read_to_string(&path) {
         Ok(text) => {
-            let ingore_case = true;
             let keyword_for_match = keyword.to_lowercase();
+            let mut matched_count = 0;
             for line in text.lines() {
-                let matched = if ingore_case {
-                    line.to_lowercase()
-                        .contains(&keyword_for_match)
+                let matched = if ignore_case {
+                    line.to_lowercase().contains(&keyword_for_match)
                 } else {
                     //as_str返回字符串的借用
                     line.contains(keyword.as_str())
                 };
                 if matched {
+                    matched_count += 1;
                     println!("{line}");
                 }
             }
+            println!("matched: {matched_count}");
         }
         Err(error) => {
             eprintln!("读取文件失败: {path},{error} ");
